6c13636198eb440444168b2e08073a0f5bc7364c
Ivan Iashkevich <i.iashkevich@astondevs.ru>
2025-04-09 21:18:01 +0500

    Initial commit

---
+++ src/main/java/com/example/authbivservice/AuthBivServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authbivservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBivServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBivServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/authbivservice/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.example.authbivservice.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/auth")
+public class AuthController {
+
+
+}
